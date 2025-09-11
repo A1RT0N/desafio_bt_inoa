99274699acf0e559f6000331db2adcca91246574
A1RT0N <ayrtonfilho@usp.br>
2025-09-11 16:26:53 +0200

    [3] main

---
+++ App/program.cs
@@ -0,0 +1,13 @@
+using System;
+
+namespace StockAlert.App
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("StockAlert iniciado...");
+            Console.WriteLine("Uso: stock-quote-alert.exe <TICKER> <precoVenda> <precoCompra>");
+        }
+    }
+}
